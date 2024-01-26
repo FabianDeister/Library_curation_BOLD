be2bdae00560614f8a654175c1232a7ce98f5e99
Rutger Vos <rutgeraldo@gmail.com>
2024-01-26 03:32:09 +0100

    now uses prepared statements

--- workflow/scripts/load_database.pl
+++ workflow/scripts/load_database.pl
@@ -10,13 +10,15 @@ my $bold_tsv;   # location of BOLD TSV dump
 my $db_file;    # where to create database file
 my $schema_sql; # location of table creation statement(s)
 my $overwrite;  # overwrite existing DB
+my $table = 'bold'; # database table
 my $log_level = 'INFO'; # verbosity level for logger
 GetOptions(
-    'tsv=s' => \$bold_tsv,
-    'db=s'  => \$db_file,
-    'sql=s' => \$schema_sql,
-    'log=s' => \$log_level,
-    'force' => \$overwrite
+    'tsv=s'   => \$bold_tsv,
+    'db=s'    => \$db_file,
+    'sql=s'   => \$schema_sql,
+    'log=s'   => \$log_level,
+    'force'   => \$overwrite,
+    'table=s' => \$table
 );
 
 # Initialize Log::Log4perl
@@ -45,19 +47,22 @@ $log->info("Going to read BOLD TSV dump $bold_tsv");
 my $tsv = Text::CSV->new({ sep_char => "\t", binary => 1, auto_diag => 1, allow_whitespace => 1, quote_char => undef });
 open my $fh, "<:encoding(utf8)", $bold_tsv or die "Could not open file '$bold_tsv': $!";
 
+# Prepare the SQL statement
+my @keys = @{ $tsv->getline($fh) };
+my $columns = join ", ", 'recordid', map { $dbh->quote($_) } @keys;
+my $count = scalar(@keys) + 1;
+my $qmarks = join ", ", ('?') x $count;
+my $sql = "INSERT INTO $table ($columns) VALUES ($qmarks)";
+my $sth = $dbh->prepare($sql) or die $dbh->errstr;
+
 # Iterate over the rows as hash refs
 my $PK = 1;
-$tsv->column_names($tsv->getline($fh));
+$tsv->column_names(\@keys);
 while (my $row = $tsv->getline_hr($fh)) {
 
-    # Make comma-separated column names and values, prefixed with primary key
-    my $columns = 'recordid,' . join ", ", map { $dbh->quote($_) } keys %$row;
-    my $values  = $PK++ . ',' . join ", ", map { $dbh->quote($_) } values %$row;
-
-    # Create insert statement and execute
-    my $table = 'bold';
-    my $sql = "INSERT INTO $table ($columns) VALUES ($values)";
-    $dbh->do($sql) or die $dbh->errstr;
+    # Insert row values prefixed with primary key
+    my @values = ( $PK++, map { $row->{$_} } @keys );
+    $sth->execute(@values) or die $dbh->errstr;
 
     # Log progress
     $log->info("Processed record $PK") unless $PK % 10_000;
