3b92a5f3b9054d834f9832d235af29d89fe8cd1c
Rutger Vos <rutgeraldo@gmail.com>
2024-01-29 11:27:54 +0100

    Update COLL_DETAILS.pm

--- lib/BCDM/Criteria/COLL_DETAILS.pm
+++ lib/BCDM/Criteria/COLL_DETAILS.pm
@@ -12,6 +12,10 @@ sub _criterion { $BCDM::Criteria::COLL_DETAILS }
 # addition, optional notes may be returned.
 # Here, the criterion to assess is:
 # 'Specimen has collection date, locality, and collector'
+# Here we need to decide what the precise BCDM fields are
+# to look at, and how to adjudicate different levels of
+# richness. E.g. is everything OK if there's just a 
+# country? Do we need multiple grades?
 sub _assess {
     my $package = shift;
     my $record = shift;
