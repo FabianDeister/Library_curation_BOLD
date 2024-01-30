4277490c72c29ba114bbf711e708a3a8d3b43cfe
Rutger Vos <rutgeraldo@gmail.com>
2024-01-30 22:53:11 +0100

    implemented string matching

--- lib/BCDM/Criteria.pm
+++ lib/BCDM/Criteria.pm
@@ -4,6 +4,7 @@ use warnings;
 use Carp 'croak';
 use BCDM::ORM;
 use Module::Load;
+use Log::Log4perl qw(:easy);
 
 our $SPECIES_ID=1;
 our $TYPE_SPECIMEN=2;
@@ -13,6 +14,15 @@ our $HAS_IMAGE=5;
 our $IDENTIFIER=6;
 our $ID_METHOD=7;
 our $COLL_DETAILS=8;
+
+
+# Initialize Log::Log4perl
+Log::Log4perl->init(\<<"END");
+  log4perl.rootLogger = INFO, Screen
+  log4perl.appender.Screen = Log::Log4perl::Appender::Screen
+  log4perl.appender.Screen.stderr = 0
+  log4perl.appender.Screen.layout = Log::Log4perl::Layout::SimpleLayout
+END
 
 # input is a bold table record and a
 # string version of the name, e.g. 'SPECIES_ID'
@@ -38,6 +48,11 @@ sub assess {
     });
 
     $result->update({ status => $status, notes => $notes });
+}
+
+sub _get_logger {
+    my ( $package, $name ) = @_;
+    return Log::Log4perl->get_logger($name);
 }
 
 1;
--- lib/BCDM/Criteria/ID_METHOD.pm
+++ lib/BCDM/Criteria/ID_METHOD.pm
@@ -7,6 +7,39 @@ use base 'BCDM::Criteria';
 # updates in the intersection table
 sub _criterion { $BCDM::Criteria::ID_METHOD }
 
+my @pos = qw(
+    descr
+    det
+    diss
+    exam
+    expert
+    genit
+    identifier
+    key
+    label
+    lit
+    micros
+    mor
+    taxonomy
+    type
+    vou
+);
+
+my @neg = qw(
+    barco
+    BOLD
+    CO1
+    COI
+    COX
+    DNA
+    mole
+    phyl
+    sequ
+    tree
+);
+
+my $log = __PACKAGE__->_get_logger(__PACKAGE__, 'DEBUG');
+
 # this tests the criterion and returns
 # boolean 0/1 depending on fail/pass. In
 # addition, optional notes may be returned.
@@ -20,8 +53,29 @@ sub _criterion { $BCDM::Criteria::ID_METHOD }
 sub _assess {
     my $package = shift;
     my $record = shift;
-    # TODO implement me
-    return 0, undef;
+    my $method = $record->identification_method;
+    my $id = $record->recordid;
+
+    # Check positive matches
+    my @mp;
+    for my $pattern ( @pos ) {
+         if ( $method =~ /$pattern/ ) {
+            push @mp, $pattern;
+            $log->info("Positive match for $id: $pattern")
+        }
+    }
+
+    # Check negative matches
+    my @mn;
+    for my $pattern ( @neg ) {
+         if ( $method =~ /$pattern/ ) {
+            push @mn, $pattern;
+            $log->info("Negative match for $id: $pattern")
+        }
+    }
+
+    # Return result
+    return @mp > @mn ? 1 : 0, "Based on pos > neg matches (@mp > @mn)";
 }
 
 1;
