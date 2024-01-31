bd864b03bc320ba4036fb88def31834305f89ca5
Rutger Vos <rutgeraldo@gmail.com>
2024-01-31 17:00:18 +0100

    implemented as per comments in #8

--- lib/BCDM/Criteria/TYPE_SPECIMEN.pm
+++ lib/BCDM/Criteria/TYPE_SPECIMEN.pm
@@ -8,8 +8,19 @@ use base 'BCDM::Criteria';
 # updates in the intersection table
 sub _criterion { $BCDM::Criteria::TYPE_SPECIMEN }
 
+my $log = __PACKAGE__->_get_logger(__PACKAGE__, 'DEBUG');
+
 sub _assess {
-    croak __PACKAGE__ . " not implemented yet";
+    my ( $class, $record ) = @_;
+    my $matches = 0;
+    my $rid = $record->recordid;
+    for my $field ( qw(extrainfo voucher_status notes) ) {
+        if ( $record->$field =~ /type/ ) {
+            $matches++;
+            $log->info("Matched $field in $rid");
+        }
+    }
+    return $matches > 0 ? 1 : 0, 'By matching /type/ against extrainfo, voucher_status, notes';
 }
 
 1;
