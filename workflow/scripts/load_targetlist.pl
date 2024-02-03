7f780d6fde45f569ce1e75485bd852e80b76cefa
Rutger Vos <rutgeraldo@gmail.com>
2024-02-03 01:02:36 +0100

    first tested version

--- workflow/scripts/load_targetlist.pl
+++ workflow/scripts/load_targetlist.pl
@@ -7,15 +7,17 @@ use Log::Log4perl qw(:easy);
 # Process command line arguments
 my $targetlist; # location of ;-separated target list
 my $db_file;    # where to create database file
-my $log_level = 'INFO'; # verbosity level for logger
-my $project = 'BGE-BIOSCAN'; # name of the project/target
+my $log_level   = 'INFO'; # verbosity level for logger
+my $project     = 'BGE-BIOSCAN'; # name of the project/target
 my $taxon_level = 'species'; # what level we're using
+my $kingdom     = 'Animalia'; # default is animals/COI-5P
 GetOptions(
     'list=s'    => \$targetlist,
     'db=s'      => \$db_file,
     'log=s'     => \$log_level,
     'project=s' => \$project,
     'taxon=s'   => \$taxon_level,
+    'kingdom=s' => \$kingdom,
 );
 
 # Initialize Log::Log4perl
@@ -34,34 +36,58 @@ $log->info("Going to connect to database $db_file");
 my $schema = BCDM::ORM->connect("dbi:SQLite:$db_file");
 
 # Start reading the target list
-$Log->info("Going to read target list $targetlist for $project");
+$log->info("Going to read target list $targetlist for $project");
 open my $fh, '<', $targetlist or die $!;
 while(<$fh>) {
     chomp;
+    next if /^$/;
 
     # Parse the line, instantiate a Target record with the
     # canonical name and the project code
     my ( $canonical, @synonyms ) = split /\s*;\s*/, $_;
-    my $target = $schema->resultset('Target')->create({
-        name        => $canonical,
-        targetlist  => $project
-    });
+    $log->debug("Instantiating target record for '$canonical'");
+    my $target = $schema->resultset('Target')->create({ name => $canonical, targetlist => $project });
+    $target->insert;
 
     # Iterate over the synonyms and instantiate Synonym
-    # records that have the foreign key
+    # records that have the foreign key to the Target
+    my %syn;
     for my $syn ( @synonyms ) {
-        $schema->resultset('Synonym')->create({
-            name     => $syn,
-            targetid => $target->id
-        });
+        $log->debug("Instantiating synonym record for '$syn'");
+        $syn{$syn} = $schema->resultset('Synonym')->create({ name => $syn, targetid => $target->targetid });
+        $syn{$syn}->insert;
     }
 
-    # TODO Matching against taxa table goes here
-    # XXX do we know what kingdom this is? What level?
+    # attempt map against taxa
+    my $taxa  = $schema->resultset('Taxa');
+    my $taxon = $taxa->search({ level => 'species', name => $canonical, kingdom => $kingdom })->single;
+    if ( not defined $taxon ) {
+        SYNONYM: for my $syn ( @synonyms ) {
+            my $t = $taxa->search({ level => 'species', name => $syn, kingdom => $kingdom })->single;
+            if ( defined $t ) {
+                $taxon = $t;
+                last SYNONYM;
+            }
+        }
+    }
+
+    # create target link
+    if ( defined $taxon ) {
+        $schema->resultset('BoldTarget')->create({
+            taxonid  => $taxon->taxonid,
+            targetid => $target->targetid,
+        })->insert;
+        $log->info("Linking target $target to taxon $taxon ($canonical)");
+    }
+    else {
+        $log->warn("No target link for $canonical");
+    }
 
     # Report progress
     $log->info("Processed record " . $target->id) unless $target->id % 1000;
 }
 
-
+# delete from synonyms;
+# delete from  bold_targets;
+# delete from targets;
 
