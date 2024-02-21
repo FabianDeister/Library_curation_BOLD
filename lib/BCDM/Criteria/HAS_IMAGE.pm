4bbe929ce80d4f3653622af986de78b2177aba26
Rutger Vos <rutgeraldo@gmail.com>
2024-02-21 13:29:14 +0100

    untested implementation of image check

--- lib/BCDM/Criteria/HAS_IMAGE.pm
+++ lib/BCDM/Criteria/HAS_IMAGE.pm
@@ -1,7 +1,13 @@
 package BCDM::Criteria::HAS_IMAGE;
 use strict;
 use warnings;
+use JSON;
+use LWP::UserAgent;
 use base 'BCDM::Criteria';
+
+my $base_url = 'http://boldsystems.org/index.php/API_Public/specimen?format=json&ids=';
+
+# http://boldsystems.org/index.php/API_Public/specimen?format=json&ids=ABINP144-21
 
 # this so that we know the criterionid for
 # updates in the intersection table
@@ -19,10 +25,32 @@ sub _criterion { $BCDM::Criteria::HAS_IMAGE }
 # and then traversing the returned JSON to look for an image. Probably an 
 # expensive operation!
 sub _assess {
-    my $package = shift;
-    my $record = shift;
-    # TODO implement me
-    return 0, undef;
+    my $package  = shift;
+    my $record   = shift;
+    my $process  = $record->processid;
+    my $wspoint  = $base_url . $process;
+    my $uagent   = LWP::UserAgent->new;
+    my $response = $uagent->get($wspoint);
+    if ( $response->is_success) {
+        my $json = $response->decoded_content;
+        my $hash = decode_json $json;
+
+        eval {
+            my $loc = $hash->{bold_records}->{records}->{$process}->{specimen_imagery}->{media}->[0]->{image_file};
+            if ( $loc ) {
+                return 1, $loc;
+            }
+            else {
+                return 0, 'no image URL in JSON';
+            }
+        };
+        if ( $@ ) {
+            return 0, $@;
+        }
+    }
+    else {
+        die $response->status_line;
+    }
 }
 
 1;
