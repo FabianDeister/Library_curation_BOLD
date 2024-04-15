0a62bdb858f0fbf82c1fc65b7f4b76f2416ff85a
@rvosa <rutgeraldo@gmail.com.com>
2024-04-15 20:13:34 +0200

    updated comments

--- workflow/scripts/make_db.sh
+++ workflow/scripts/make_db.sh
@@ -7,6 +7,7 @@ SCHEMA=./schema.sql
 INDEXES=./indexes.sql
 LIBS=../../lib
 
+# 1. CREATE DATABASE AND LOAD BCDM
 # Creates the empty database, reads BCDM TSV
 # and dumps it into the database. This would become a
 # Snakefile rule, with inputs, outputs, a shell 
@@ -19,6 +20,11 @@ perl load_bcdm.pl \
   --log=INFO \
   --force
 
+# 2. LOAD CURATION CRITERIA
+# Here the contents of criteria.tsv need to be loaded
+# into the database.
+
+# 3. APPLY INDEXES
 # Applies additional indexes to the database. Same here,
 # this would become a rule. Probably needs an additional
 # output so that snakemake knows the rule was done. If
@@ -27,21 +33,22 @@ perl load_bcdm.pl \
 # do this every time.
 sqlite3 $DB_FILE < $INDEXES
 
+# 4. PRODUCE OBJECT-RELATIONAL MAPPING
 # Exports the database schema as Perl modules. This is
 # probably not needed as a Snakefile rule each time,
 # because this code now exists already. Only if the 
 # database schema changes, this would have to be rerun.
 dbicdump -o dump_directory=$LIBS BCDM::ORM "dbi:SQLite:dbname=$DB_FILE"
 
-# Here comes a script that uses the ORM to populate the 
+#  5. Here comes a script that uses the ORM to populate the 
 # normalized taxonomy tree. This would be a Snakefile rule.
 perl -I${LIBS} load_taxonomy.pl --db=$DB_FILE --log=INFO
 
-# Here comes a script that imports the target list, 
+# 6. Here comes a script that imports the target list, 
 # its synonyms, and mapping to taxonomy tree. Again a 
 # Snakefile rule.
 
-# Here the script to assess criteria. Possibly the
+# 7. Here the script to assess criteria. Possibly the
 # same script is invoked by multiple, separate Snakefile
 # rules, e.g. one for each criterion:
 # COLLECTION_DATE
@@ -66,11 +73,8 @@ for C in $CRITERIA; do
 		--criteria=${C} 2> ${C}.log > ${C}.tsv
 done
 
-# Possibly there will be a need to aggregate and
-# summarise the outcomes from the different criteria
-# into a single, graded value.
-
-# And finally there would be a rule/script here 
+# 8. And finally there would be a rule/script here 
 # that outputs filtered data in BCDM.
-
+# This is done according to the instructions and
+# query in pivot.sql
 
