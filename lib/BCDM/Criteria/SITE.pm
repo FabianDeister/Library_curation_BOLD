9705fa65822dc63548b19395c0808679ed75363e
Ben Price <bwprice@users.noreply.github.com>
2024-04-20 11:34:08 +0100

    Update SITE.pm
    revert to original but replaced 'None' with ''

--- lib/BCDM/Criteria/SITE.pm
+++ lib/BCDM/Criteria/SITE.pm
@@ -13,8 +13,8 @@ sub _criterion { $BCDM::Criteria::SITE }
 sub _assess {
     my $package = shift;
     my $record = shift;
-    return ($record->site =~ /^\s*$/) ? (0, "Determined from blank site column") : (1, "Determined from site column");
-    # original: return $record->site eq 'None' ? 0 : 1, "Determined from site column";
+    return $record->site eq '' ? 0 : 1, "Determined from site column";
+# alternative regex: return ($record->site =~ /^\s*$/) ? (0, "Determined from blank site column") : (1, "Determined from site column");
 }
 
 1;
