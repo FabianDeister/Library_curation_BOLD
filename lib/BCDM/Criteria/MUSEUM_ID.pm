734b7b55c2e4e12c5bed4f1cb2248fdbda8eb2f4
Ben Price <bwprice@users.noreply.github.com>
2024-04-20 11:36:47 +0100

    Update MUSEUM_ID.pm
    revert to original approach, replacing 'None' with ''

--- lib/BCDM/Criteria/MUSEUM_ID.pm
+++ lib/BCDM/Criteria/MUSEUM_ID.pm
@@ -13,8 +13,8 @@ sub _criterion { $BCDM::Criteria::MUSEUM_ID }
 sub _assess {
     my $package = shift;
     my $record = shift;
-# original search: return $record->museumid eq 'None' ? 0 : 1, "Determined from museumid column";
-    return ($record->museumid =~ /^\s*$/) ? (0, "Determined from blank museumid column") : (1, "Determined from museumid column");
+    return $record->museumid eq '' ? 0 : 1, "Determined from museumid column";
+    #option using regex: return ($record->museumid =~ /^\s*$/) ? (0, "Determined from blank museumid column") : (1, "Determined from museumid column");
 }
 
 1;
