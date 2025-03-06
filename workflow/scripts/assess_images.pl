bb2f01c73d47353310b7b268cec8503f4ab12c15
StephanKuehbandner <stephan.kuehbandner@web.de>
2025-03-06 16:21:14 +0100

    use BOLD v5 API instead of BOLD v3 API
    BOLD v3 API has been deprecated. Now, it needs a query ID to query images. query ID can be opbtained from query with process ID

--- workflow/scripts/assess_images.pl
+++ workflow/scripts/assess_images.pl
@@ -10,18 +10,17 @@ use Getopt::Long;
 use Time::HiRes qw(usleep);
 use Log::Log4perl qw(:easy);
 
-# number of microsends to sleep before requests. Can be adjusted from outside the module via something like 500:
-# $BCDM::Criteria::HAS_IMAGE::SLEEP = 500;
+# Delay to prevent overloading the API (adjustable)
 our $SLEEP = 500;
 
-# endpoints to the CAOS object store. I wonder if the port number is fixed.
-my $base_url  = 'https://caos.boldsystems.org:31488/api/images?processids=';
-my $image_url = 'https://caos.boldsystems.org:31488/api/objects/';
+# New API Endpoints
+my $query_api  = 'https://portal.boldsystems.org/api/query?query=';
+my $image_api  = 'https://portal.boldsystems.org/api/images/';
 
 # Process command line arguments
-my $db_file;  # where to access database file
-my $tsv_file; # using the raw TSV instead
-my $log_level = 'INFO'; # verbosity level for logger
+my $db_file;
+my $tsv_file;
+my $log_level = 'INFO';
 my $persist   = 0;
 GetOptions(
     'db=s'       => \$db_file,
@@ -40,87 +39,69 @@ END
 
 # Instantiate logger
 my $log = Log::Log4perl->get_logger('assess_criteria');
-$log->info("Going to check for images");
+$log->info("Starting image assessment...");
 
 # Instantiate user agent
 my $ua = LWP::UserAgent->new;
-$log->info("Instantiated user agent $ua");
+$log->info("Initialized user agent");
 
-# Connect to the database. Because there is a column `order` in the BCDM, we need to
-# escape this. In SQLite that's with double quotes.
+# Connect to the database or TSV
 my $io;
-if ( $db_file ) {
-    $log->info("Going to connect to database $db_file");
-    $io = BCDM::IO->new( db => $db_file );
+if ($db_file) {
+    $log->info("Connecting to database $db_file");
+    $io = BCDM::IO->new(db => $db_file);
 }
-elsif ( $tsv_file) {
-    $log->info("Going to open TSV file $tsv_file");
-    $io = BCDM::IO->new( tsv => $tsv_file );
+elsif ($tsv_file) {
+    $log->info("Opening TSV file $tsv_file");
+    $io = BCDM::IO->new(tsv => $tsv_file);
 }
 
 # Iterate over all provided records
 $io->prepare_rs;
-{
-	my @queue;
-	while (my $record = $io->next) {
-
-		# queue is full
-		if (scalar(@queue) == 100) {
-			eval { process_queue(@queue) };
-			die $@ if $@;
-
-			# flush queue, wait a bit
-			@queue = ();
-			usleep($SLEEP);
-		}
-		else {
-
-			# add record to queue
-			push @queue, $record;
-		}
-	}
-
-	# process remaining records
-	process_queue(@queue);
+while (my $record = $io->next) {
+    eval { process_record($record) };
+    if ($@) {
+        $log->error("Error processing record: $@");
+    }
+    usleep($SLEEP);
 }
 
-sub process_queue {
-	my @queue = @_;
+sub process_record {
+    my ($record) = @_;
+    my $pid = $record->processid;
+    my $rid = $record->recordid;
 
-	# create GET request URL, attempt to access it
-	my $process = join ',', map { $_->processid } @queue;
-	my $wspoint = $base_url . $process;
-	$log->debug("Attempting $wspoint");
-	my $response = $ua->get($wspoint);
-	$log->debug($response);
+    # Query API for a single processid
+    my $query_url = $query_api . "ids:processid:" . $pid . "&extent=limited";
+    $log->debug("Querying BOLD API: $query_url");
 
-	# proceed if the HTTP::Response object signals success
-	if ( $response->is_success ) {
+    my $query_response = $ua->get($query_url);
+    unless ($query_response->is_success) {
+        die "Query API failed: " . $query_response->status_line . "\nResponse: " . $query_response->decoded_content;
+    }
 
-		# fetch content
-		my $json = $response->decoded_content;
-		my $array_ref = decode_json $json;
-		$log->debug(Dumper($array_ref));
+    my $query_json = decode_json($query_response->decoded_content);
+    my $query_id   = $query_json->{query_id} or die "No query_id returned!";
 
-		# iterate over the current queue, check for each record if there is an entry in the JSON response.
-		# If so, print the record ID and the URL to the image. If not, print the record ID and a sad smiley.
-		for my $record (@queue) {
-			my $pid = $record->processid;
-			my $rid = $record->recordid;
-			my ($match) = grep {$_->{processid} eq $pid} @$array_ref;
-			my @result = ( $rid, $BCDM::Criteria::HAS_IMAGE );
-			if ($match) {
-				push @result, 1, $image_url . $match->{objectid};
-			}
-			else {
-				push @result, 0, ':-(';
-			}
-			print join( "\t", @result ), "\n";
-		}
-	}
+    # Fetch images using the query_id
+    my $image_url = $image_api . $query_id . '?max_images=-1';
+    $log->debug("Fetching images: $image_url");
 
-	# die if the HTTP::Response object signals failure
-	else {
-		die $response->status_line;
-	}
+    my $image_response = $ua->get($image_url);
+    unless ($image_response->is_success) {
+        die "Image API failed: " . $image_response->status_line . "\nResponse: " . $image_response->decoded_content;
+    }
+
+    my $image_json = decode_json($image_response->decoded_content);
+    my $image_data = $image_json->{images} || [];
+
+    my ($match) = grep { $_->{processid} eq $pid } @$image_data;
+    my @result = ($rid, $BCDM::Criteria::HAS_IMAGE);
+
+    if ($match) {
+        push @result, 1, $match->{image_url};
+    } else {
+        push @result, 0, 'No image found';
+    }
+    print join("\t", @result), "\n";
 }
